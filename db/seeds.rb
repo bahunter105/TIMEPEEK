146db68cc4bf1b619b7336c177bb6c8d43f2476e
Junichi Masuda <junichimasuda@outlook.com>
2021-11-15 18:15:12 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleanig DB"
+Offer.destroy_all
+
+
+puts "Creating Offers"
+
+6.times do
+  Offer.create(name: Faker::Movies::BackToTheFuture.character,
+    price: rand(1000..9000),
+    description: Faker::Movies::Lebowski.quote,
+    category: Faker::Beer.style,
+    rating: rand(1..5),
+    max_travellers: rand(1..4))
+end
+
+puts "done"
