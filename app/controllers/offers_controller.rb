d051bb9a3b06a389dfaa134479bd8eba304773c3
Gonzalo Guerra <cryptogon@outlook.com>
2021-11-16 17:32:08 +0100

    show page1

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,10 +1,8 @@
 class OffersController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
-
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @offers = Offer.all
   end
-
   def show
     @offer = Offer.find(params[:id])
   end
