98ec0f8fd7f9571aaf35b3f3ebd6431a94b79ba8
Gonzalo Guerra <cryptogon@outlook.com>
2021-11-17 14:40:39 +0100

    show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -40,3 +40,25 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+
+const channels = require.context('.', true, /_channel\.js$/)
+channels.keys().forEach(channels)
+
+const parentContainer = document.querySelector('.read-more-container');
+
+parentContainer.addEventListener('click', event => {
+
+  const current = event.target;
+
+  const isReadMoreBtn = current.className.includes('read-more-btn');
+
+  if (!isReadMoreBtn) return;
+
+  const currentText = event.target.parentNode.querySelector('.read-more-text');
+
+  currentText.classList.toggle('read-more-text--show');
+
+  current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
+
+})
