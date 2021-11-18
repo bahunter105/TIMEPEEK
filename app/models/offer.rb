80bd0debe83284fbb3aa0655c437c49829338696
Hunter Bourgeois <bahunter@gmail.com>
2021-11-18 17:36:28 +0100

    search bar functioning

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,5 +3,16 @@ class Offer < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
   ### enter validations!!!
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+    against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+    pg_search_scope :search_by_category,
+    against: [:category],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 
 end
