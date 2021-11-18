2d1a9eab0f343e6bd1e3405ff712cce08cfb3755
Hunter Bourgeois <bahunter@gmail.com>
2021-11-18 10:37:37 +0100

    updated the new booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,14 +2,19 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
   end
+
   def create
+    @offer = Offer.find(params[:offer_id])
     @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.offer = @offer
     if @booking.save
       redirect_to offer_path(@offer)
     else
-      render :new
+      render "offers/show"
     end
   end
+
   private
   def booking_params
     params.require(:booking).permit(:no_of_travellers, :date, :user, :offer)
